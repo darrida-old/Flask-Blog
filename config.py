bf6151b937d717d5d88a1e44fcc77d646d077480
darrida <darridapy@gmail.com>
2019-11-18 00:47:05 -0600

    Revert "update"
    This reverts commit 5db4dc70d96565d51148f60d116ca58c40e1e132.

--- config.py
+++ config.py
@@ -28,13 +28,13 @@ class Config:
     
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ('DEV_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
         
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ('TEST_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite://'
     WTF_CSRF_ENABLED = False
         
