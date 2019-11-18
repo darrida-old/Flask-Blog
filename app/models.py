41d614bd26fc044a9fb8c55c64febf0ce626f75d
darrida <darridapy@gmail.com>
2019-11-17 23:09:31 -0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -139,7 +139,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    default = db.Column(db.Boolean, default=False, index=True)
+    default = db.Column(db.Boolean, default=False)
     permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
