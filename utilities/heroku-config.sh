06e7c55224ecac7ce7c1226dda6314e9483dbdc6
darrida <darridapy@gmail.com>
2019-11-23 18:13:14 -0600

    try again

--- utilities/heroku-config.sh
+++ utilities/heroku-config.sh
@@ -7,6 +7,7 @@
 sudo heroku config:set FLASK_APP=flasky.py
 sudo heroku addons:create heroku-postgresql:hobby-dev
 sudo heroku config:set FLASK_CONFIG=heroku
+sudo heroku config:set FLASKY_ADMIN=bentestflask@gmail.com
 sudo heroku config:set SECRET_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 sudo heroku config:set MAIL_USERNAME=bentestflask@gmail.com
 sudo heroku config:set MAIL_PASSWORD=$(read -p 'MAIL_PASSWORD: ' uservar)
