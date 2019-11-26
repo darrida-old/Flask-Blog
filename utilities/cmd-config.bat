f94081ec39af90c31baf4563e589f732f5cc47fc
darrida <darridapy@gmail.com>
2019-11-26 13:30:03 -0600

    readme update, dev config batch file

---
+++ utilities/cmd-config.bat
@@ -0,0 +1,5 @@
+set FLASK_APP=flasky.py
+set FLASK_CONFIG=production
+set MAIL_USERNAME=bentestflask@gmail.com
+set MAIL_PASSWORD=n7H{X]Mnr%,Dd5ep[Rko1P29C0qN]P
+set FLASKY_ADMIN=bentestflask@gmail.com
