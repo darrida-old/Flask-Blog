ca4df7bd44c6d8696b89d00f0cf652a0f2cbbef8
darrida <darridapy@gmail.com>
2020-01-07 22:24:58 -0600

    added echo statements to ubuntu-config.sh

--- tools/ubuntu-config.sh
+++ tools/ubuntu-config.sh
@@ -5,10 +5,15 @@
 #
 ##############################################
 export FLASK_APP=flasky.py
+echo "FLASK_APP=flasky.py"
 export FLASK_CONFIG=development
+echo "FLASK_CONFIG=development"
 export FLASKY_ADMIN=bentestflask@gmail.com
+echo "FLASKY_ADMIN=bentestflask@gmail.com"
 export SECRET_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+echo "SECRET_KEY set"
 export MAIL_USERNAME=bentestflask@gmail.com
+echo "MAIL_USERNAME=bentestflask@gmail.com"
 echo ""
 echo "Ubuntu configuration complete."
 echo "***Manually set MAIL_PASSWORD*** - using 'set MAIL_PASSWORD=[password]'"
