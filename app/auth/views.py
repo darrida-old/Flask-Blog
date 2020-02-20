3f646fd5290eaee4f0c4c815f2b0b190442d4b28
darrida <darridapy@gmail.com>
2020-02-19 18:40:19 -0600

    disabled registered page

--- app/auth/views.py
+++ app/auth/views.py
@@ -29,20 +29,20 @@ def logout():
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
 
-@auth.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email=form.email.data, username=form.username.data,
-                    password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        token = user.generate_confirmation_token()
-        send_email(user.email, 'Confirm Your Account', 'auth/email/confirm',
-                   user=user, token=token)
-        flash('A confirmation email has been sent to you by email.')
-        return redirect(url_for('main.index'))
-    return render_template('auth/register.html', form=form)
+# @auth.route('/register', methods=['GET', 'POST'])
+# def register():
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         user = User(email=form.email.data, username=form.username.data,
+#                     password=form.password.data)
+#         db.session.add(user)
+#         db.session.commit()
+#         token = user.generate_confirmation_token()
+#         send_email(user.email, 'Confirm Your Account', 'auth/email/confirm',
+#                    user=user, token=token)
+#         flash('A confirmation email has been sent to you by email.')
+#         return redirect(url_for('main.index'))
+#     return render_template('auth/register.html', form=form)
 
 @auth.route('/confirm/<token>')
 @login_required
